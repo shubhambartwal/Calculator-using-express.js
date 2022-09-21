f575a900f8a1aa035e716c3fd0b364a60a3d3e2a
Shubham Bartwal <shubhambartwal58@gmail.com>
2022-09-21 23:51:49 +0530

    bmi calculator

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -14,5 +14,15 @@ app.post("/",function(req,res){
     res.send("The result of the calculation is "+result);
 });
 app.listen(3000,function(){
-    console.log("Server on 3000.");
+    console.log("Server on 3000."); 
+});
+
+app.get("/bmicalculator",function(req,res){
+    res.sendFile(__dirname+"/bmiCalculator.html");
+});
+app.post("/bmicalculator",function(req,res){
+    var weight=parseFloat(req.body.weight);
+    var height=parseFloat(req.body.height);
+var bmi=weight/(height*height);
+res.send ("Your BMI is "+ bmi);
 });
